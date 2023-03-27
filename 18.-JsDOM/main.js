5ad915c46681fba9e11471654056b61bd39dc1da
Alejandro Grimaldo <alejandrogrim12@gmail.com>
2023-03-27 14:37:05 -0600

    jsdom

--- 18.-JsDOM/main.js
+++ 18.-JsDOM/main.js
@@ -1,18 +1,19 @@
-const todos = [];
+const todos = []
+
 window.onload = () => {
-  const form = document.getElementById("todo-form");
+  const form = document.getElementById('todo-form')
   form.onsubmit = (e) => {
-    e.preventDefault();
-    const todo = document.getElementById("todo");
-    const todoText = todo.value;
-    todo.value = "";
-    todos.push(todoText);
-    const todoList = document.getElementById("todo-list");
-    const todosTemplate = todos.map((t) => "<li>" + t + "</li>");
-    todoList.innerHTML = todosTemplate.join("");
-    const elementos = document.querySelectorAll("#todo-list li");
+    e.preventDefault()
+    const todo = document.getElementById('todo')
+    const todoText = todo.value
+    todo.value = ''
+    todos.push(todoText)
+    const todoList = document.getElementById('todo-list')
+    const todosTemplate = todos.map((t) => '<li>' + t + '</li>')
+    todoList.innerHTML = todosTemplate.join('')
+    const elementos = document.querySelectorAll('#todo-list li')
     elementos.forEach((elementos, i) => {
-      console.log(elemento, i);
-    });
-  };
-};
+      console.log(elementos, i)
+    })
+  }
+}

d6a74e76d77f3ad830bc1d31bdd9b3c8ff6f98b2
Alejandro Grimaldo <alejandrogrim12@gmail.com>
2023-04-07 08:59:38 -0600

    terminado

--- 18.-JsDOM/main.js
+++ 18.-JsDOM/main.js
@@ -1,6 +1,27 @@
-const todos = []
+const todos = JSON.parse(localStorage.getItem('todos')) || []
+
+const render = () => {
+  const todoList = document.getElementById('todo-list')
+  const todosTemplate = todos.map((t) => '<li>' + t + '</li>')
+  todoList.innerHTML = todosTemplate.join('')
+  const elementos = document.querySelectorAll('#todo-list li')
+  elementos.forEach((elemento, i) => {
+    elemento.addEventListener('click', () => {
+      elemento.parentNode.removeChild(elemento)
+      todos.splice(i, 1)
+      actualizaTodos()
+      render()
+    })
+  })
+}
+
+const actualizaTodos = () => {
+  const todoStrings = JSON.stringify(todos)
+  localStorage.setItem('todos', todoStrings)
+}
 
 window.onload = () => {
+  render()
   const form = document.getElementById('todo-form')
   form.onsubmit = (e) => {
     e.preventDefault()
@@ -8,12 +29,7 @@ window.onload = () => {
     const todoText = todo.value
     todo.value = ''
     todos.push(todoText)
-    const todoList = document.getElementById('todo-list')
-    const todosTemplate = todos.map((t) => '<li>' + t + '</li>')
-    todoList.innerHTML = todosTemplate.join('')
-    const elementos = document.querySelectorAll('#todo-list li')
-    elementos.forEach((elementos, i) => {
-      console.log(elementos, i)
-    })
+    actualizaTodos()
+    render()
   }
 }

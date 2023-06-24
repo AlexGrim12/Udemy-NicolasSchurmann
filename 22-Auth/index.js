9b75f09c090f3a383c605f2edf617f9397b5c49d
Alejandro Grimaldo <alejandrogrim12@gmail.com>
2023-06-24 00:35:46 -0600

    terminado

--- 22-Auth/index.js
+++ 22-Auth/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const expressJwt = require('express-jwt')
+const { expressjwt: expressJwt } = require('express-jwt')
 const User = require('./user')
 
 mongoose.connect(
@@ -13,9 +13,13 @@ const app = express()
 
 app.use(express.json())
 
-const signToken = (_id) => jwt.sign({ _id }, 'secret')
+const validateJwt = expressJwt({
+  secret: 'mi-string-secreto',
+  algorithms: ['HS256'],
+})
+const signToken = (_id) => jwt.sign({ _id }, 'mi-string-secreto')
 
-app.post(`/register`, async (req, res) => {
+app.post('/register', async (req, res) => {
   const { body } = req
   console.log({ body })
   try {
@@ -44,20 +48,47 @@ app.post(`/login`, async (req, res) => {
     const user = await User.findOne({ email: body.email })
     if (!user) {
       return res.status(403).send('User does not exist')
-    }
-    else {
+    } else {
       const isMatch = await bcrypt.compare(body.password, user.password)
       if (isMatch) {
         const signed = signToken(user._id)
         return res.status(200).send({ user, signed })
-      }
-      else {
+      } else {
         return res.status(403).send('Wrong user or password')
       }
     }
   } catch (err) {
     res.status(500).send(err.message)
   }
+})
+const findAndAssignUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.auth._id)
+    if (!user) {
+      return res.status(401).end()
+    }
+    req.auth = user
+    next()
+  } catch (err) {
+    next(err)
+  }
+}
+
+const isAuthenticaded = express.Router().use(validateJwt, findAndAssignUser)
+
+app.get('/lele', isAuthenticaded, (req, res) => {
+  throw new Error('Nuevo Error')
+  res.send(req.auth)
+})
+
+app.use((err, req, res, next) => {
+  console.error('Mi nuevo error', err.stack)
+  next(err)
+})
+
+app.use((err, req, res, next) => {
+  res.send('Ha ocurrido un error :(')
+  next(err)
 })
 
 app.listen(3000, () =>
